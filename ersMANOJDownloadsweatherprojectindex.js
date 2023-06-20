fdb16e33c76c8a9dec2937d6a658fb84da8ecd7b
unknown <manojanthati26@gmail.com>
2023-06-20 20:29:44 +0530

    changed into a perfectly working model

---
+++ ersMANOJDownloadsweatherprojectindex.js
@@ -0,0 +1,11 @@
+[33mcommit e240855c81e1add3e939cdd78bb94dcb738d53bc[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: unknown <manojanthati26@gmail.com>
+Date:   Fri Jun 16 19:13:15 2023 +0530
+
+    changes are we updated port
+
+[33mcommit f3ac71dc8a39547689ab9084c1e62167bad0cc18[m
+Author: unknown <manojanthati26@gmail.com>
+Date:   Fri Jun 16 19:01:50 2023 +0530
+
+    first commit without hosting any server
--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+      let  name  = "Warangal";
+      let description  = "little cloudy";
+      let icon = "https://openweathermap.org/img/wn/04n.png";
+      let temp  = "31";
+      let humidity = "30";
+      let  speed  = "5";
+app.set('view engine','ejs');
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req,res){
     
-    res.sendFile(__dirname+"/index.html");
+    res.render("list",{weathericon:icon,city:name,temperature:temp,weatherdescription:description,humid:humidity,windspeed:speed});
 })
 
 app.post("/",function(req,res){
@@ -18,34 +26,31 @@ app.post("/",function(req,res){
     const unit = "metric";
     const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit;
     https.get(url,function(response){
-        response.on("data",function(data){
-           const weatherData = JSON.parse(data);
-        //    console.log(weatherData);
-        //    const temp = weatherData.main.temp;
-      const  name  = weatherData.name;
-      const description  = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
-      const temp  = weatherData.main.temp;
-      const humidity = weatherData.main.humidity;
-      const  speed  = weatherData.wind.speed;
-      // document.querySelector(".city").innerText = "Weather in " + name;
-      // document.querySelector(".icon").src =
-      //   "https://openweathermap.org/img/wn/" + icon + ".png";
-      // document.querySelector(".description").innerText = description;
-      // document.querySelector(".temp").innerText = temp + "°C";
-      // document.querySelector(".humidity").innerText =
-      //   "Humidity: " + humidity + "%";
-      // document.querySelector(".wind").innerText =
-      //   "Wind speed: " + speed + " km/h";
-      // document.querySelector(".weather").classList.remove("loading");
-      // document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + ")";
-      res.write("<h1>weather in "+name+" "+description+"</h1>");
-      res.write("<h3>and temperature is "+temp+" humidity is "+humidity+"</h3>");
-      res.write("<img src=https://openweathermap.org/img/wn/"+icon+"@2x.png>");
-       res.send();
-           
-        })
+        if(response.statusCode === 200){
+            response.on("data",function(data){
+                const weatherData = JSON.parse(data);
+             //    console.log(weatherData);
+             //    const temp = weatherData.main.temp;
+            name  = weatherData.name;
+            description  = weatherData.weather[0].description;
+            const icon1 = weatherData.weather[0].icon;
+            icon="https://openweathermap.org/img/wn/" + icon1+ "@2x.png";
+            temp  = weatherData.main.temp;
+            humidity = weatherData.main.humidity;
+            speed  = weatherData.wind.speed;
+            res.redirect("/");   
+             })
+        }
+        else {
+            res.sendFile(__dirname+"/failure.html");
+        }
+       
+        
+       
     })
+})
+app.post("/failure",function(req,res){
+    res.redirect("/");
 })
 
 
